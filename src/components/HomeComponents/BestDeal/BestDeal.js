7d99cc621b3e94bf574450459c61da6d37b9aa02
unknown <rjmahir.faisal@gmail.com>
2022-05-15 11:56:16 +0600

    header and navigation complete design

--- src/components/HomeComponents/BestDeal/BestDeal.js
+++ src/components/HomeComponents/BestDeal/BestDeal.js
@@ -3,7 +3,7 @@ import React from 'react';
 const BestDeal = () => {
     return (
         <div>
-            <h2 className='text-3xl'>This is best deal</h2>
+            <h2 className='text-3xl text-[#3e3c3c] font-medium my-10'>Find Your Best Deal</h2>
         </div>
     );
 };
--- src/components/HomeComponents/Header/Header.js
+++ src/components/HomeComponents/Header/Header.js
@@ -1,54 +1,77 @@
 import React from 'react';
+import loan from './images/loan.PNG';
+import deposit from './images/deposit.PNG';
+import creditCard from './images/creditCard.PNG';
+import insurance from './images/insurance.PNG';
+import priceBond from './images/priceBond.PNG';
+import Image from 'next/image';
+import { BsPerson } from 'react-icons/bs';
+import { AiOutlineCar } from 'react-icons/ai';
+import { BiHomeAlt } from 'react-icons/bi';
+import { BiCalendar } from 'react-icons/bi';
+import { RiMedicineBottleLine } from 'react-icons/ri';
+import { MdOutlineSavings } from 'react-icons/md';
+import { RiExchangeDollarLine } from 'react-icons/ri';
+import { IoIosGitCompare } from 'react-icons/io';
+import { MdOutlinePriceChange } from 'react-icons/md';
+
+
 
 const Header = () => {
+
+
     return (
-        <div className='h-[90vh] bg-[#1b5f8d] grid place-items-center'>
-            <h2 className='text-5xl text-white'>Compare Rates, Check Eligibility & Apply</h2>
+        <div className='h-[92vh] bg-[#1b5f8d] grid place-items-center'>
+            <div className="max-w-[1200px] mx-auto ">
+                <h2 className='max-w-[1000px] mx-auto text-4xl md:text-5xl xl:text-6xl text-white mb-10'>Compare Rates, Check Eligibility & Apply</h2>
+                <div className="grid grid-cols-5 gap-1">
 
-            <div className="max-w-[1200px] mx-auto flex items-center justify-center gap-1">
+                    <div className="header-card-container">
+                        <Image src={loan} alt="Image" className=" header-card-image" />
 
-                <div className="header-card-container">
-                    <img src="https://i.ibb.co/Hx5Pwbc/banner-4.png" alt="Image" className=" header-card-image min-h-[250px]" />
-
-                    <div className="header-card-overlay">
-                        <h2> Faisal </h2>
-                        <h5> i am faisal </h5>
+                        <div className="header-card-overlay">
+                            <a href=""><BsPerson color='lightblue' size="28px" /> Personal Loan</a>
+                            <a href=""><BiHomeAlt color='orange' size="28px" /> Home Loan</a>
+                            <a href=""><AiOutlineCar color='rgb(118, 193, 14)' size="28px" /> Car Loan</a>
+                        </div>
                     </div>
-                </div>
-                <div className="header-card-container">
-                    <img src="https://i.ibb.co/Hx5Pwbc/banner-4.png" alt="Image" className=" header-card-image min-h-[250px]" />
+                    <div className="header-card-container">
+                        <Image src={deposit} alt="Image" className=" header-card-image" />
 
-                    <div className="header-card-overlay">
-                        <h2> Faisal </h2>
-                        <h5> i am faisal </h5>
+                        <div className="header-card-overlay">
+                            <a href=""><RiMedicineBottleLine color='lightblue' size="28px" /> DPS</a>
+                            <a href=""><RiMedicineBottleLine color='orange' size="28px" /> Fixed Deposit</a>
+                            <a href=""><BiCalendar color='lightblue' size="28px" /> Monthly Benefit</a>
+                            <a href=""><MdOutlineSavings color='lightblue' size="28px" /> Savings Account</a>
+                            <a href=""><RiExchangeDollarLine color='rgb(218, 193, 14)' size="28px" /> Double Mony</a>
+                            <a href=""><RiExchangeDollarLine color='rgb(118, 193, 14)' size="28px" /> Triple Mony</a>
+                        </div>
                     </div>
-                </div>
-                <div className="header-card-container">
-                    <img src="https://i.ibb.co/Hx5Pwbc/banner-4.png" alt="Image" className=" header-card-image min-h-[250px]" />
+                    <div className="header-card-container">
+                        <Image src={creditCard} alt="Image" className=" header-card-image" />
 
-                    <div className="header-card-overlay">
-                        <h2> Faisal </h2>
-                        <h5> i am faisal </h5>
+                        <div className="header-card-overlay">
+                            <a href=""><IoIosGitCompare color='lightblue' size="28px" /> Compare Rate</a>
+                            <a href=""><IoIosGitCompare color='orange' size="28px" /> Check Eligibility</a>
+                        </div>
                     </div>
-                </div>
-                <div className="header-card-container">
-                    <img src="https://i.ibb.co/Hx5Pwbc/banner-4.png" alt="Image" className=" header-card-image min-h-[250px]" />
+                    <div className="header-card-container">
+                        <Image src={insurance} alt="Image" className=" header-card-image" />
 
-                    <div className="header-card-overlay">
-                        <h2> Faisal </h2>
-                        <h5> i am faisal </h5>
+                        <div className="header-card-overlay">
+                            <a href=""><AiOutlineCar color='lightblue' size="28px" /> Motor Insurance</a>
+                        </div>
                     </div>
-                </div>
-                <div className="header-card-container">
-                    <img src="https://i.ibb.co/Hx5Pwbc/banner-4.png" alt="Image" className=" header-card-image min-h-[250px]" />
+                    <div className="header-card-container">
+                        <Image src={priceBond} alt="Image" className=" header-card-image" />
 
-                    <div className="header-card-overlay">
-                        <h2> Faisal </h2>
-                        <h5> i am faisal </h5>
+                        <div className="header-card-overlay">
+                            <a href=""><MdOutlinePriceChange color='rgb(118, 193, 14)' size="28px" /> Price Bond</a>
+                        </div>
                     </div>
+
+
                 </div>
-
-
             </div>
         </div>
     );
